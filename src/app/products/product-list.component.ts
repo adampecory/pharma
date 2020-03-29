904e5e8a4b619a5d0078a4326d76d7f16d227f17
adampecory <adampecory@gmail.com>
2020-03-29 14:27:13 +0200

    linting

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,34 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector : 'pm-productlist',
+    selector : 'app-productlist',
     templateUrl : './product-list.component.html'
 })
 export class ProducListComponent {
-    title : string = "Liste des médicaments";
-    imgUrl : string = 'assets\\images\\medoc.jpg';
-    imgWidth : number = 100;
-    imgShow : boolean = false;
-    filteredValue : string ='dol';
-    
-    medocs : any[] = [
+    title: 'Liste des médicaments';
+    imgUrl: 'assets\\images\\medoc.jpg';
+    imgWidth: 100;
+    imgShow: false;
+    filteredValue: 'dol';
+
+    medocs: any[] = [
         {
-            nom:'Doliprane 500mg',
+            nom: 'Doliprane 500mg',
             dateAchat: '01/01/2020',
             dateExpiration: '31/12/2020',
             estOuvert: false,
             description: 'Mots de têtes, douleurs'
         },
         {
-            nom:'Efferalgan 1000',
+            nom: 'Efferalgan 1000',
             dateAchat: '02/01/2020',
             dateExpiration: '31/12/2021',
             estOuvert: true,
             description: 'Effervescent Most de têtes, douleurs'
         }
-    ]
+    ];
 
-    showImage() : void {
+    showImage(): void {
         this.imgShow = !this.imgShow;
     }
 }
