2c9a7a1a3717a2d474d4675caa14d759120a78f0
adampecory <adampecory@gmail.com>
2020-04-01 15:02:56 +0200

    Standard routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,34 @@ import { ProducListComponent } from './products/product-list.component';
 import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
+import { ProductDetailComponent } from './products/product-detail.component';
+import { HomeComponent } from './home/home.component';
+const appRoutes: Routes = [
+  { path: 'detail/:id', component: ProductDetailComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'medocs', component: ProducListComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', redirectTo: 'home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ProducListComponent,
     ConvertToSpacePipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule, 
-    HttpClientModule
+    HttpClientModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
